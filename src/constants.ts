440ee90066b375c5775a38e1f3b10f44743b7d15
Janis Horsts <janis.horsts@gmail.com>
2025-09-21 09:54:05 +0100

    chore: ensure consistent style (#33)
    * chore(renovate): added :pinVersions
    
    * fix: test amd64 or arm64

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export enum State {
-  CachePrimaryKey = "EARTHLY_CACHE_KEY",
-  CacheMatchedKey = "EARTHLY_CACHE_RESULT",
-  BinaryPath = "EARTHLY_BINARY_PATH",
+  CachePrimaryKey = 'EARTHLY_CACHE_KEY',
+  CacheMatchedKey = 'EARTHLY_CACHE_RESULT',
+  BinaryPath = 'EARTHLY_BINARY_PATH',
 }
 
 export enum Outputs {
-  CacheHit = "cache-hit",
+  CacheHit = 'cache-hit',
 }
